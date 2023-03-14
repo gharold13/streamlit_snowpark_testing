a4ae2387dea7dd91d2f563cfe738d6a7414679cf
Giovanni Harold <104081304+gharold13@users.noreply.github.com>
2023-03-14 12:08:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import snowflake.connector as sf
+import snowflake.connector
 # import snowflake.snowpark.session as Session
 
 """
