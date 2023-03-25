6993bbb3f2123551058e53e7fa1e08da6bce55e1
Giovanni Harold <gharold@2ndwatch.com>
2023-03-25 13:12:51 -0500

    fixing __main__

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,6 @@ st.set_page_config(
      }
 )
 
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
 # Create Session object
 def create_session_object():
     connection_parameters = st.secrets["snowflake"]
@@ -78,26 +70,6 @@ def load_data(session):
             pd_df_co2_top_n = snow_df_co2.filter(col('Total CO2 Emissions') > emissions_threshold).to_pandas()
             st.bar_chart(data=pd_df_co2_top_n.set_index('Location Name'), width=850, height=500, use_container_width=True)
 
-if __name__ == "__main__":
-    session = create_session_object()
-    load_data(session)
 
-
-"""
-def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
-
-conn = init_connection()
-
-@st.cache_data
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        dat = cur.fetchall()
-        df = pd.DataFrame(dat, columns=[col[0] for col in cur.description])
-        return df
-
-df = run_query("SELECT * from FOOD_INSPECTIONS_FULL limit 500")
-"""
+session = create_session_object()
+load_data(session)
